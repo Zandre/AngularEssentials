83e1fe47944d10325b6ae56fb710d5f906a0a24c
Zandre Botha <botha.zandre@gmail.com>
2019-11-25 11:49:29 +0200

    Section 2 - two-way binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { UserComponent } from './user.component';
+
 
 @NgModule({
   declarations: [
@@ -10,9 +12,11 @@ import { UserComponent } from './user.component';
     UserComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
