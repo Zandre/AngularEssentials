42e396ae9a68bfdf58a564b529a60a5be87dbdfb
Zandre Botha <botha.zandre@gmail.com>
2019-11-25 13:02:20 +0200

    Section 2 - binding component properties

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,16 +1,16 @@
-import {Component} from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `
   <input type="text" [(ngModel)]="name">
   <p>Hello {{name}}!</p>
-  <p>I am the user component</p>
+  <p>I'm the user component</p>
   `
 })
 
 export class UserComponent {
-  name = 'Zandre';
+  @Input () name;
 
   onUserInput(event){
     this.name = event.target.value;
